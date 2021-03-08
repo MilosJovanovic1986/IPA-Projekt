982d4331a8c14da9fba47a94ed20c5b9d8ba6462
Milos Jovanovic <jovanovicm@gmx.ch>
2021-03-08 14:21:17 +0100

    cleanup socket.js file

--- frontends/socket.js
+++ frontends/socket.js
@@ -1,14 +1,6 @@
 import io from "socket.io-client";
 
-// This socket use for connection to backend
+// Connection to the backend
 const socket = io("http://localhost:7000");
 
 export default socket;
-// const io = require("socket.io")(httpServer, {
-//     cors: {
-//       origin: "http://localhost:7000",
-//       methods: ["GET", "POST"]
-//     }
-//   });
-
-//   httpServer.listen(7000);
